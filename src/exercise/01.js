de1c2b3fa112d011b811d0f950c3d7c8934fb5a0
RiczCalixto <rcalixtos@gmail.com>
2023-06-04 21:58:39 -0300

    feat: complete exercise 1

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -3,10 +3,12 @@
 
 import * as React from 'react'
 // 💣 remove this import
-import Globe from '../globe'
+// import Globe from '../globe'
 
 // 🐨 use React.lazy to create a Globe component which uses a dynamic import
 // to get the Globe component from the '../globe' module.
+const loadGlobe = () => import(/* webpackPrefetch: true */ '../globe')
+const LazyGlobe = React.lazy(loadGlobe)
 
 function App() {
   const [showGlobe, setShowGlobe] = React.useState(false)
@@ -16,28 +18,34 @@ function App() {
   // 💰 try putting it in a few different places and observe how that
   // impacts the user experience.
   return (
-    <div
-      style={{
-        display: 'flex',
-        alignItems: 'center',
-        flexDirection: 'column',
-        justifyContent: 'center',
-        height: '100%',
-        padding: '2rem',
-      }}
-    >
-      <label style={{marginBottom: '1rem'}}>
-        <input
-          type="checkbox"
-          checked={showGlobe}
-          onChange={e => setShowGlobe(e.target.checked)}
-        />
-        {' show globe'}
-      </label>
-      <div style={{width: 400, height: 400}}>
-        {showGlobe ? <Globe /> : null}
+    <React.Suspense fallback={<div>loading...</div>}>
+      <div
+        style={{
+          display: 'flex',
+          alignItems: 'center',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          height: '100%',
+          padding: '2rem',
+        }}
+      >
+        <label
+          style={{marginBottom: '1rem'}}
+          onMouseEnter={loadGlobe}
+          onMouseOver={loadGlobe}
+        >
+          <input
+            type="checkbox"
+            checked={showGlobe}
+            onChange={e => setShowGlobe(e.target.checked)}
+          />
+          {' show globe'}
+        </label>
+        <div style={{width: 400, height: 400}}>
+          {showGlobe ? <LazyGlobe /> : null}
+        </div>
       </div>
-    </div>
+    </React.Suspense>
   )
 }
 // 🦉 Note that if you're not on the isolated page, then you'll notice that this
